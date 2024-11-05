cdc86a66b82020dd4a5d44a62175ccc952081883
talhagozutok <talhagozutok@users.noreply.github.com>
2024-11-05 20:31:53 +0300

    Introduce Board class

--- EightPuzzle.Console/Program.cs
+++ EightPuzzle.Console/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EightPuzzle;
+
+int[,] initialBoard = {
+    { 7, 2, 4 },
+    { 5, 0, 6 },
+    { 8, 3, 1 }
+};
+
+Board board = new(initialBoard);
+
+var zeroPosition = board.IndexOfZero();
+Console.WriteLine($"Index of zero: ({zeroPosition.Item1}, {zeroPosition.Item2})");
+
+int[,] sameBoard = {
+    { 7, 2, 4 },
+    { 5, 0, 6 },
+    { 8, 3, 1 }
+};
+Console.WriteLine("Is equal to sameBoard: " + board.IsEqual(sameBoard));
+
+int[,] differentBoard = {
+    { 1, 2, 3 },
+    { 4, 5, 0 },
+    { 6, 7, 8 }
+};
+Console.WriteLine("Is equal to differentBoard: " + board.IsEqual(differentBoard));
+
+Console.WriteLine("Hashcode: " + board.GetHashCode());
+
+Board anotherBoard = new(sameBoard);
+Console.WriteLine("Equals anotherBoard: " + board.Equals(anotherBoard));
---
+++ EightPuzzle/Board.cs
@@ -0,0 +1,82 @@
+﻿namespace EightPuzzle;
+
+public class Board
+{
+    public int[,] Tiles { get; set; }
+
+    public Board(int[,] tiles)
+    {
+        Tiles = (int[,])tiles.Clone();
+    }
+
+    // Find the index of zero (ie. empty tile)
+    public (int, int) IndexOfZero()
+    {
+        for (int i = 0; i < Tiles.GetLength(0); i++)
+        {
+            for (int j = 0; j < Tiles.GetLength(1); j++)
+            {
+                if (Tiles[i, j] == 0)
+                {
+                    return (i, j);
+                }
+            }
+        }
+
+        return (-1, -1); // If there is no zero in the board
+    }
+
+    // Check equality of two boards
+    public bool IsEqual(int[,] tilesToTest)
+    {
+        for (int i = 0; i < Tiles.GetLength(0); i++)
+        {
+            for (int j = 0; j < Tiles.GetLength(1); j++)
+            {
+                if (Tiles[i, j] != tilesToTest[i, j])
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    // Generate a unique hash code for the board state
+    public override int GetHashCode()
+    {
+        int hash = 0;
+        int multiplier = 1;
+
+        for (int i = 0; i < Tiles.GetLength(0); i++)
+        {
+            for (int j = 0; j < Tiles.GetLength(1); j++)
+            {
+                hash += Tiles[i, j] * multiplier;
+                multiplier *= 10;
+            }
+        }
+        return hash;
+    }
+
+    // Check if this board is equal to another board
+    public override bool Equals(object obj)
+    {
+        if (obj is not Board otherBoard)
+        {
+            return false;
+        }
+
+        for (int i = 0; i < Tiles.GetLength(0); i++)
+        {
+            for (int j = 0; j < Tiles.GetLength(1); j++)
+            {
+                if (Tiles[i, j] != otherBoard.Tiles[i, j])
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
